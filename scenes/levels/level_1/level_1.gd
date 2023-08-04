7fe075a9420d80d02c1b9f504f0cf4f35c92a5e7
Dmitriy770 <zdima08@gmail.com>
2023-08-04 23:50:33 +0300

    refactor level_1

--- scenes/levels/level_1/level_1.gd
+++ scenes/levels/level_1/level_1.gd
@@ -3,49 +3,40 @@ extends Node2D
 @onready var tile_map := $TileMap as TileMap
 @onready var tile_counter_label := $Label as Label
 
-var mouse_left_down: bool = false
 var tile_counter := 15
 
-func _ready():
+func _ready() -> void:
 	update_tile_counter(tile_counter)
 
-func update_tile_counter(mount):
+func update_tile_counter(mount: int):
 	tile_counter_label.text = str(mount)
 	tile_counter = mount
 
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("action"):
+		draw_ground()
 
-func _input(event: InputEvent):
-	if event is InputEventMouseButton:
-		if event.button_index == 1 and event.is_pressed():
-			mouse_left_down = true
-		elif event.button_index == 1 and not event.is_pressed():
-			mouse_left_down = false
-				
-func _process(some_change):
+func draw_ground() -> void:
+	var mouse_pos := get_global_mouse_position()
+	var mouse_pos_map := tile_map.local_to_map(mouse_pos)
 	
-	if mouse_left_down:
-		var mouse_pos := get_global_mouse_position()
-		var mouse_pos_map := tile_map.local_to_map(mouse_pos)
-		
-		var layer := 1
-		var coords := [
-			mouse_pos_map,
-			Vector2i(mouse_pos_map[0] - 1, mouse_pos_map[1]),
-			Vector2i(mouse_pos_map[0] + 1, mouse_pos_map[1]),
-			Vector2i(mouse_pos_map[0], mouse_pos_map[1] + 1),
-			Vector2i(mouse_pos_map[0], mouse_pos_map[1] - 1),
-			Vector2i(mouse_pos_map[0] + 1, mouse_pos_map[1] + 1),
-			Vector2i(mouse_pos_map[0] + 1, mouse_pos_map[1] - 1),
-			Vector2i(mouse_pos_map[0] - 1, mouse_pos_map[1] - 1),
-			Vector2i(mouse_pos_map[0] - 1, mouse_pos_map[1] + 1)
-		]
-		var terrain_set = 0
-		var terrain = 1
-		
-		var tile_data := tile_map.get_cell_tile_data(layer, mouse_pos_map)
-		
-		if tile_data != null and tile_data.get_custom_data("is_can_build"):
-			tile_map.set_cells_terrain_connect(layer, coords, terrain_set, terrain, true)
-		
-			update_tile_counter(tile_counter - 1)
-		
+	var layer := 1
+	var coords := [
+		mouse_pos_map,
+		Vector2i(mouse_pos_map[0] - 1, mouse_pos_map[1]),
+		Vector2i(mouse_pos_map[0] + 1, mouse_pos_map[1]),
+		Vector2i(mouse_pos_map[0], mouse_pos_map[1] + 1),
+		Vector2i(mouse_pos_map[0], mouse_pos_map[1] - 1),
+		Vector2i(mouse_pos_map[0] + 1, mouse_pos_map[1] + 1),
+		Vector2i(mouse_pos_map[0] + 1, mouse_pos_map[1] - 1),
+		Vector2i(mouse_pos_map[0] - 1, mouse_pos_map[1] - 1),
+		Vector2i(mouse_pos_map[0] - 1, mouse_pos_map[1] + 1)
+	]
+	var terrain_set = 0
+	var terrain = 1
+	
+	var tile_data := tile_map.get_cell_tile_data(layer, mouse_pos_map)
+	
+	if tile_data == null or tile_data.get_custom_data("is_can_build"):
+		tile_map.set_cells_terrain_connect(layer, coords, terrain_set, terrain, true)
+		update_tile_counter(tile_counter - 1)
