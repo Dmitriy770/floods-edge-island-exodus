212b9f57c19adc8d5e28c4f2a0cf908c1489d1b0
dearsley <ganohin431@gmail.com>
2023-08-05 20:33:32 +0300

    scene transition animation, added level infomation in selection level scene

--- animations/exit_animation/ExitAnimation.gd
+++ animations/exit_animation/ExitAnimation.gd
@@ -5,3 +5,9 @@ func exit():
 	$AnimationPlayer.play('dissolve')
 	await $AnimationPlayer.animation_finished
 	get_tree().quit()
+
+func change_scene(location: String):
+	$AnimationPlayer.play('dissolve')
+	await $AnimationPlayer.animation_finished
+	get_tree().change_scene_to_file(location)
+	$AnimationPlayer.play_backwards('dissolve')
