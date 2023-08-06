d000421957ea7951b7db9e937c1c5c8ec7e3dc22
Dmitriy770 <zdima08@gmail.com>
2023-08-06 17:33:14 +0300

    add level_selection functionality

--- scenes/scenes/level_selection/level_selection.gd
+++ scenes/scenes/level_selection/level_selection.gd
@@ -1,3 +1,55 @@
-extends Node2D
+extends Control
+
+var all_levels: Array[MenuItem] = []
+var current_level_ind := 0
+
+@onready var header_label := %Header as Label
+@onready var description_label := %Description as Label
+@onready var status_label := %StatusLabel as Label
+@onready var time_label := %TimeLabel as Label
+@onready var tile_label := %TileLabel as Label
+
+@onready var items_container := $MenuItems as Node2D
+
+func _ready() -> void:
+	for item in items_container.get_children() as Array[MenuItem]:
+		item.index = len(all_levels)
+		item.level_selected.connect(on_level_selected)
+		all_levels.append(item)
+	check_levels_state()
+	update_level_window()
+	all_levels[current_level_ind].grab_focus()
+	
+func update_level_window() -> void:
+	var current_level := all_levels[current_level_ind]
+	var level_data := current_level.level_data
+	
+	header_label.text = current_level.level_name
+	description_label.text = current_level.description
+	status_label.text = get_string_state(level_data.state)
+	tile_label.text = str(level_data.transit_time)
+	tile_label.text = str(level_data.amount_spent_tiles)
+
+func get_string_state(state: LevelData.States) -> String:
+	match state:
+		LevelData.States.OPEN:
+			return "Open"
+		LevelData.States.CLOSE:
+			return "Close"
+		LevelData.States.PASSED:
+			return "Passed"
+	return ""
+
+func on_level_selected(index: int) -> void:
+	current_level_ind = index
+	update_level_window()
+
+func check_levels_state() -> void:
+	for i in range(0, len(all_levels) - 1):
+		if all_levels[i].level_data.state == LevelData.States.PASSED \
+			and all_levels[i+1].level_data.state == LevelData.States.CLOSE:
+			all_levels[i+1].change_state(LevelData.States.OPEN)
 
 
+func _on_start_game_button_pressed() -> void:
+	all_levels[current_level_ind].start_game()
--- scenes/scenes/level_selection/menu_items/menu_item/menu_item.gd
+++ scenes/scenes/level_selection/menu_items/menu_item/menu_item.gd
@@ -1,25 +1,38 @@
+class_name MenuItem
 extends Node2D
 
-enum States {
-	OPEN,
-	CLOSE,
-	PASSED,
-}
+signal level_selected(index: int)
 
 const SPRITES := {
-	States.OPEN: preload("res://assets/level_open_marker.png"),
-	States.CLOSE: preload("res://assets/level_close_marker.png"),
-	States.PASSED: preload("res://assets/level_passed_marker.png"),
+	LevelData.States.OPEN: preload("res://assets/level_open_marker.png"),
+	LevelData.States.CLOSE: preload("res://assets/level_close_marker.png"),
+	LevelData.States.PASSED: preload("res://assets/level_passed_marker.png"),
 }
 
-var state := States.CLOSE
+@export var level_name := "level"
+@export var description := "description"
+@export var level_scene := preload("res://scenes/levels/level/level.tscn") as PackedScene
+
+var level_data: LevelData = null
+var index := 0
 
 @onready var sprite := $Sprite2D as Sprite2D
+@onready var button := $Button as Button
 
 func _ready() -> void:
-	# state = [данные из БД]
+	level_data = LevelData.new() # [данные из БД]
 	pass
 
-func change_state(new_state: States) -> void:
-	state = new_state
-	sprite = SPRITES[state]
+func change_state(new_state: LevelData.States) -> void:
+	level_data.state = new_state
+	sprite.texture = SPRITES[level_data.state]
+
+
+func _on_button_toggled(_button_pressed: bool) -> void:
+	level_selected.emit(index)
+
+func grab_focus() -> void:
+	button.grab_focus()
+
+func start_game() -> void:
+	SceneManager.change_scene(level_scene)
