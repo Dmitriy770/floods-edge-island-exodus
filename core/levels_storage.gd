b4d2d686bdaa94c11b41d8e95bed454f37640c71
MaximM <metsomax@mail.ru>
2023-08-06 17:26:17 +0300

    added mechanics to save progress; minor fixes to the level selection scene

--- core/levels_storage.gd
+++ core/levels_storage.gd
@@ -1,7 +1,54 @@
 extends Node
 
+const  SAVE_GAME_FILE := "user://savegame.save"
+
+
 func get_level_data(name: String) -> LevelData:
 	return LevelData.new()
 	
 func save_level_data(level: LevelData) -> void:
-	pass
+	var old_levels_data := load_level_data()
+
+	var save_game_file = FileAccess.open(SAVE_GAME_FILE, FileAccess.WRITE)
+	
+	if save_game_file == null:
+		printerr("save failed with error code {0}".format([FileAccess.get_open_error()]))
+		return
+	
+	old_levels_data[level.name] = {
+		"state": level.state,
+		"transit_time": level.transit_time,
+		"amount_spent_tiles": level.amount_spent_tiles,
+		}
+	
+	var json_object := JSON.new()
+	save_game_file.store_line(json_object.stringify(old_levels_data))
+	
+func load_level_data() -> Dictionary:
+	if not FileAccess.file_exists(SAVE_GAME_FILE):
+		return {}
+		
+	var save_game_file = FileAccess.open(SAVE_GAME_FILE, FileAccess.READ)
+	
+	if save_game_file == null:
+		printerr("save failed with error code {0}".format([FileAccess.get_open_error()]))
+		return {}
+	
+	var json_object := JSON.new()
+	var error = json_object.parse(save_game_file.get_line())
+	
+	if error != OK:
+		return {}
+	
+	var data = json_object.get_data()
+	return data
+
+func reset_all_levels_data():
+	var save_game_file = FileAccess.open(SAVE_GAME_FILE, FileAccess.WRITE)
+	
+	if save_game_file == null:
+		printerr("save failed with error code {0}".format([FileAccess.get_open_error()]))
+		return
+	
+	var json_object := JSON.new()
+	save_game_file.store_line(json_object.stringify({}))
--- objects_data/level_data.gd
+++ objects_data/level_data.gd
@@ -5,5 +5,5 @@ enum States {OPEN, CLOSE, PASSED}
 
 var name := ""
 var state := States.CLOSE
-var transit_time := 0.0
+var transit_time := 0
 var amount_spent_tiles := 0
