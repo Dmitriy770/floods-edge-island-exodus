fe93a673df742d26b7f1861fb9a8e107bf6246fa
Dmitriy770 <zdima08@gmail.com>
2023-08-06 22:24:36 +0300

    add education, add description, and some fix

---
+++ scenes/levels/level_1/level_1.gd
@@ -0,0 +1,23 @@
+extends Level
+
+var counter := 0
+var education_end := false
+
+@onready var label1 := $Education/Control/PanelContainer/Label as Label
+@onready var label2 := $Education/Control/PanelContainer/Label2 as Label
+@onready var education := $Education as CanvasLayer
+
+func _ready():
+	super._ready()
+	label1.show()
+	label2.hide()
+
+func _input(event: InputEvent) -> void:
+	if not education_end and event.is_action_pressed("action"):
+		counter +=1
+		if counter == 1:
+			label1.hide()
+			label2.show()
+		elif counter > 1:
+			education_end = true
+			education.queue_free()
