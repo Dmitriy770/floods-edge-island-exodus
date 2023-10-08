2ae04d7c276883f57631bcdd339ad4e6da33bb95
Metso Maxim <metsomax@mail.ru>
2023-10-08 15:30:47 +0300

    added a level passing time calculation; minor changes

--- scenes/levels/level/end_game_overlay/end_game_overlay.gd
+++ scenes/levels/level/end_game_overlay/end_game_overlay.gd
@@ -13,7 +13,7 @@ var is_open := false
 func _ready():
 	hide()
 
-func show_overlay(result: bool, transit_time: int, amount_spent_tiles: int, scene_name: SceneManager.Scenes) -> void:
+func show_overlay(result: bool, transit_time: float, amount_spent_tiles: int, scene_name: SceneManager.Scenes) -> void:
 	if is_open:
 		return
 	is_open = true
@@ -26,7 +26,7 @@ func show_overlay(result: bool, transit_time: int, amount_spent_tiles: int, scen
 	else:
 		header_label.text = "НЕУДАЧА"
 	
-	time_label.text = str(transit_time)
+	time_label.text = time_to_str(transit_time)
 	tile_label.text = str(amount_spent_tiles)
 
 func save_level_data(level_name: String, is_win: bool, time: float, tiles: int) -> void:
@@ -39,6 +39,7 @@ func save_level_data(level_name: String, is_win: bool, time: float, tiles: int) 
 		data.state = LevelData.States.PASSED
 		data.amount_spent_tiles = min(old_data.amount_spent_tiles if old_data.amount_spent_tiles != 0 else tiles, tiles)
 		data.transit_time = min(old_data.transit_time, time)
+		
 		LevelsStorage.save_level_data(data)
 
 
@@ -48,3 +49,9 @@ func _on_start_game_button_pressed():
 
 func _on_reset_game_button_pressed():
 	SceneManager.change_scene(current_scene)
+
+
+func time_to_str(time: float) -> String:
+	var secs := fmod(time, 60)
+	var mins := fmod(time, 60*60) / 60
+	return "%02d:%02d" % [mins, secs]
